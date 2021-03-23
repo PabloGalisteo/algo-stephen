db7c95c86eb9f34de2aa75ae3b98aaae647bbfab
Pablo Galisteo <pablogalisteo86@gmail.com>
2021-03-23 18:10:56 +0100

    third exercise. Reverse a number

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,10 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    return Math.sign(n) < 0 ? 
+    -parseInt(n.toString().split('').reverse().join('')) :
+     parseInt(n.toString().split('').reverse().join(''))
+}
 
 module.exports = reverseInt;
